67dc2bd992bf5999f6267253d7b6ad11810d815b
Eugene <eleges@ya.ru>
2023-03-15 11:24:45 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
+# Import libraries
+import pandas as pd
+import numpy as np
+import streamlit as st
 
-# import pandas as pd
-# import numpy as np
-# import streamlit as st
 
-# st.title ("this is the app title")
 # st.header("this is the markdown")
 # st.markdown("this is the header")
 # st.subheader("this is the subheader")
@@ -12,17 +12,32 @@
 # st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
 
-# df= pd.DataFrame(np.random.randn(10, 2),columns=['x', 'y'])
+df = pd.DataFrame(
+    np.random.randn(10, 2),
+    columns=['x', 'y']
+    )
 # st.line_chart(df)
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-
 # Set page title
-st.set_page_config(page_title="My Streamlit App")
+# st.set_page_config(page_title="My Streamlit App")
+st.title ("My Streamlit App")
 
 # Set sidebar options
 st.sidebar.title("Menu")
 option = st.sidebar.selectbox("Select an option", ("Option 1", "Option 2", "Option 3"))
 
+# Create some charts
+if option == "Option 1":
+    st.title("Option 1")
+    # st.header("this is the markdown")
+    # st.markdown("this is the header")
+    # st.subheader("this is the subheader")
+    st.write("This is option 1.")
+    st.line_chart(df)
+elif option == "Option 2":
+    st.title("Option 2")
+    st.write("This is option 2.")
+    st.bar_chart(df)
+elif option == "Option 3":
+    st.title("Option 3")
+    st.write("This is option 3.")
