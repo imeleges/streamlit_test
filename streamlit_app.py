72b94892e3bd8dcd2cb507516211b09a1f4ba2e7
Eugene <eleges@ya.ru>
2023-03-15 10:50:19 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 
+import pandas as pd
+import numpy as np
 import streamlit as st
 
 st.title ("this is the app title")
@@ -8,3 +10,7 @@ st.subheader("this is the subheader")
 st.caption("this is the caption")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+
+df= pd.DataFrame(np.random.randn(10, 2),columns=['x', 'y'])
+st.line_chart(df)
